9b5bfa38828a683bcbb6bb14a3eab74c3e600588
張子濤 <b05703092@ntu.edu.tw>
2021-08-19 11:04:32 +0800

    Updated TWENDS.cpp to O(n) memory

--- SPOJ/TWENDS.cpp
+++ SPOJ/TWENDS.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 #define MAXN 1003
-int a[MAXN], dp[MAXN][MAXN];
+int a[MAXN], dp[MAXN][2];
 
 
 int main(){
     int n, g = 0;
     while(1){
+        memset(dp, 0, sizeof(dp));
         scanf("%d", &n);
         if(!n) break;
         for(int i = 0; i < n; ++i) scanf("%d", &a[i]);
-        for(int l = 2; l <= n; ++l){
+        int x = 0;
+        for(int l = 2; l <= n; l+=2){
+            x=1-x;
             for(int i = 0; i+l-1 < n; ++i){
                 int j = i+l-1;
-                int left = a[i] + (a[i+1] >= a[j] ? dp[i+2][j]-a[i+1] : dp[i+1][j-1]-a[j]);
-                int right = a[j] + (a[i] >= a[j-1] ? dp[i+1][j-1]-a[i] : dp[i][j-2]-a[j-1]);
-                dp[i][j] = max(left, right);
+                int left = a[i] + (a[i+1] >= a[j] ? dp[i+2][1-x]-a[i+1] : dp[i+1][1-x]-a[j]);
+                int right = a[j] + (a[i] >= a[j-1] ? dp[i+1][1-x]-a[i] : dp[i][1-x]-a[j-1]);
+                dp[i][x] = max(left, right);
             }
         }
-        printf("In game %d, the greedy strategy might lose by as many as %d points.\n", ++g, dp[0][n-1]);
+        printf("In game %d, the greedy strategy might lose by as many as %d points.\n", ++g, dp[0][x]);
     }
     return 0;
 }
